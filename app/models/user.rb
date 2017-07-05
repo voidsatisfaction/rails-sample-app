a306fb472898fc8045f70fecb2b0967c0603ec05
voidSatisfaction <lourie@naver.com>
2017-07-05 15:07:39 +0900

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ApplicationRecord
   validates :password,
     presence: true,
     length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
